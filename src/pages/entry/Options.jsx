ec7e76cd4556c7d0ad117bed6b36bee5b1453424
Priscila Falcão <priscila_falcao@dell.com>
2023-07-18 15:00:31 -0300

    updated tests to include new ones, updated Options and Order Entry to fix an error on the tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -15,10 +15,19 @@ export default function Options({ optionType }) {
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
+    // create an abortController to attach to network request
+    const controller = new AbortController();
     axios
-      .get(`http://localhost:3030/${optionType}`)
+      .get(`http://localhost:3030/${optionType}`, { signal: controller.signal })
       .then((response) => setItems(response.data))
-      .catch((error) => setError(true));
+      .catch((error) => {
+        if (error.name !== "CanceledError") setError(true);
+      });
+
+    // abort axios call on component unmount
+    return () => {
+      controller.abort();
+    };
   }, [optionType]);
 
   if (error) {
